593894a452215744126ed7b1650e6617c42819e4
deBroglie <tanski.gustaw@gmail.com>
2020-03-21 17:51:59 +0100

    LaTeX site changed and caching introduced

--- Assets/Scripts/Application/Model/Cube/TextureDownloader.cs
+++ Assets/Scripts/Application/Model/Cube/TextureDownloader.cs
@@ -1,4 +1,9 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
 using BetterMultidimensionalArray;
 using Data;
 using UnityEngine;
@@ -6,18 +11,19 @@ using UnityEngine.Networking;
 
 public class LaTeXTextureDownloader {
 
-    private const string baseURL = @"https://latex.codecogs.com/png.latex?";
+    private const string url = @"http://sciencesoft.at/latex";
     private string[, , ] laTeXTensor;
     private TextureSettings settings;
+    private static Dictionary<string, Task<Texture2D>> fetchedTextures = new Dictionary<string, Task<Texture2D>>();
 
     public static Task<Texture2D[, , ]> Fetch(string[, , ] laTeX, TextureSettings? settings) {
         LaTeXTextureDownloader downloader = new LaTeXTextureDownloader(laTeX, settings);
-        return downloader.Fetch();
+        return downloader.GetTextures();
     }
 
     public static Task<Texture2D[, , ]> Fetch(string[, , ] laTeX) {
         LaTeXTextureDownloader downloader = new LaTeXTextureDownloader(laTeX, null);
-        return downloader.Fetch();
+        return downloader.GetTextures();
     }
 
     private LaTeXTextureDownloader(string[, , ] laTeX, TextureSettings? settings) {
@@ -25,19 +31,60 @@ public class LaTeXTextureDownloader {
         this.settings = settings ?? new TextureSettings() { size = Size.normal };
     }
 
-    private Task<Texture2D[, , ]> Fetch() {
-        return laTeXTensor.Select(FetchIndexTexture).WaitForAll();
+    private Task<Texture2D[, , ]> GetTextures() {
+        return laTeXTensor.Select(GetTexture).WaitForAll();
     }
 
-    private async Task<Texture2D> FetchIndexTexture(string laTeX) {
-        UnityWebRequest www;
-        www = UnityWebRequestTexture.GetTexture(baseURL + DecorateLaTeX(laTeX));
+    private Task<Texture2D> GetTexture(string latex) {
+        string decoratedLaTeX = DecorateLaTeX(latex);
+        if (!fetchedTextures.ContainsKey(decoratedLaTeX)) {
+            fetchedTextures[decoratedLaTeX] = FetchTexture(decoratedLaTeX);
+        }
+        return fetchedTextures[decoratedLaTeX];
+    }
+
+    private string DecorateLaTeX(string laTeX) {
+        return @"\huge\({\color{white}" + laTeX + @"}\)";
+    }
+
+    private async Task<Texture2D> FetchTexture(string laTeX) {
+        UnityWebRequest www = GeneratePutRequest(laTeX);
+        await www.SendWebRequest();
+        www = UnityWebRequestTexture.GetTexture(GetUrlFromResponse(www));
         await www.SendWebRequest();
         return DownloadHandlerTexture.GetContent(www);
     }
 
-    private string DecorateLaTeX(string laTeX) {
-        return @"\dpi{999}" + LatexDictionaries.size[settings.size] + @"{\color{white}" + laTeX + "}";
+    private UnityWebRequest GeneratePutRequest(string laTeX) {
+        UnityWebRequest request = UnityWebRequest.Put(url, GenerateLaTeXRequestBody(laTeX));
+        request.SetRequestHeader("Content-Type", @"application/xml");
+        return request;
+    }
+
+    private byte[] GenerateLaTeXRequestBody(string laTeX) {
+        return Encoding.Default.GetBytes(GenerateXmlString(laTeX));
+    }
+
+    private string GenerateXmlString(string laTeX) {
+        return @"<?xml version=""1.0"" encoding=""UTF-8""?>  
+        <latex ochem=""false"" bgcolor=""#000"">
+            <dev dpi=""600"">pngalpha</dev>
+            <src><![CDATA[\documentclass[20pt]{article}
+                \usepackage{amssymb,amsmath,xcolor}
+                \pagestyle{empty}
+                \begin{document}
+                " + laTeX + @"
+                \end{document}]]>
+            </src>
+            <embeddedData>false</embeddedData>
+        </latex>";
+    }
+
+    private string GetUrlFromResponse(UnityWebRequest www) {
+        string xmlString = Encoding.UTF8.GetString(www.downloadHandler.data);
+        XDocument doc = XDocument.Parse(xmlString);
+        XElement urlElement = doc.Descendants("url").First();
+        return urlElement.Value;
     }
 
 }
