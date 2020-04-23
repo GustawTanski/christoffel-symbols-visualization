89f1d974dfb8d7bb6730a59175b8fcc8d8d3e951
deBroglie <tanski.gustaw@gmail.com>
2020-04-23 16:04:39 +0200

    minicube's indexes respond to changes of space type

--- Assets/Scripts/Application/View/FlyingCamera/MiniCube/Arrow/ArrowView.cs
+++ Assets/Scripts/Application/View/FlyingCamera/MiniCube/Arrow/ArrowView.cs
@@ -3,10 +3,19 @@ using UnityEngine;
 public class ArrowView : ChristofellElement {
     public DynamicSprite indexSprite;
     public uint index;
-
     private string laTeX;
     private Texture2D texture;
     private async void Start() {
+        SetListeners();
+        await SetIndexTexture();
+    }
+
+    private void SetListeners() {
+        App.spaceChangedEvent.listOfHandlers += OnSpaceChanged;
+    }
+
+    private async void OnSpaceChanged(object caller, SpaceChangedArgs e) {
+        await Task.Delay(100);
         await SetIndexTexture();
     }
 
