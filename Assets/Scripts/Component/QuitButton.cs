df04ba4d804df9ebb01d9ecafe630969b6f248a9
deBroglie <tanski.gustaw@gmail.com>
2020-05-03 16:40:24 +0200

    Quit Button added

---
+++ Assets/Scripts/Component/QuitButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+
+public class QuitButton : MonoBehaviour {
+    private void Awake() {
+        GetComponent<Button>().onClick.AddListener(() => Application.Quit(0));
+    }
+}
