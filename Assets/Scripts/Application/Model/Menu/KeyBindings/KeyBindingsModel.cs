04250766df2a5501f30312de293e9d990d8228b0
deBroglie <tanski.gustaw@gmail.com>
2020-07-16 13:28:59 +0200

    Added few example bindings to play with and test

--- Assets/Scripts/Application/Model/Menu/KeyBindings/KeyBindingsModel.cs
+++ Assets/Scripts/Application/Model/Menu/KeyBindings/KeyBindingsModel.cs
@@ -1,3 +1,12 @@
-public class KeyBindingsModel: ChristofellElement {
-    
-}
+using System.Collections.Generic;
+public class KeyBindingsModel : ChristofellElement {
+
+    public KeyBinding MenuToggle { get; } = new KeyBinding("escape", "Menu Toggle");
+    public KeyBinding Accelerate { get; } = new KeyBinding("leftShift", "Accelerate");
+    public KeyBinding Decelerate { get; } = new KeyBinding("leftControl", "Decelerate");
+    public List<KeyBinding> KeyBindings { get; private set; }
+
+    private void Awake() {
+        KeyBindings = new List<KeyBinding> { MenuToggle, Accelerate, Decelerate };
+    }
+};
