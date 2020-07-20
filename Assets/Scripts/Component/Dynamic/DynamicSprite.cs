d716875f721516bfd07dfe9cb2e54095ad9ae8e9
deBroglie <tanski.gustaw@gmail.com>
2020-07-20 19:55:58 +0200

    Too big labels are now scaled down to fit in element

--- Assets/Scripts/Component/Dynamic/DynamicSprite.cs
+++ Assets/Scripts/Component/Dynamic/DynamicSprite.cs
@@ -15,8 +15,22 @@ public class DynamicSprite : MonoBehaviour {
         }
     }
 
+    public int dimensionLimit = int.MaxValue;
+
+    private Vector3 initialScale;
+
+    private void Awake() {
+        initialScale = transform.localScale;
+    }
+
     public void SetTexture(Texture2D texture) {
         GetComponent<SpriteRenderer>().sprite = SpriteCreator.Create(texture);
+        int biggerDimension = texture.width > texture.height ? texture.width : texture.height;
+        if ((float) biggerDimension / 100 > dimensionLimit) {
+            transform.localScale = initialScale * ((float) dimensionLimit * 100 / biggerDimension) * 0.95f;
+        } else {
+            transform.localScale = initialScale;
+        }
     }
 
     public void ToggleAppear() {
