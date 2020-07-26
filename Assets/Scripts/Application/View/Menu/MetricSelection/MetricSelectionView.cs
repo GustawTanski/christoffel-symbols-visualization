9e283b6bdca17b320cd83361e7d3475103cd2e27
deBroglie <tanski.gustaw@gmail.com>
2020-07-26 11:06:40 +0200

    Apply Button created

--- Assets/Scripts/Application/View/Menu/MetricSelection/MetricSelectionView.cs
+++ Assets/Scripts/Application/View/Menu/MetricSelection/MetricSelectionView.cs
@@ -1,11 +1,12 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MetricSelectionView: MenuElement {
     public ParametersPanelView parametersPanel;
     public TMP_Dropdown dropdown;
-
     public TMP_Text spacetimeName;
+    public Button applyButton;
     public GameObject warning;
 
 }
