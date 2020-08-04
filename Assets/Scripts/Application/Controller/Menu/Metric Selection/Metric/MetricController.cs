9649ea54a92bd2bcc1676946c3b0962ef534c5bd
deBroglie <tanski.gustaw@gmail.com>
2020-08-04 18:01:48 +0200

    MetricController cleaned

--- Assets/Scripts/Application/Controller/Menu/Metric Selection/Metric/MetricController.cs
+++ Assets/Scripts/Application/Controller/Menu/Metric Selection/Metric/MetricController.cs
@@ -1,22 +1,63 @@
-using UnityEngine;
 using System.Linq;
+using System.Threading.Tasks;
 using BetterMultidimensionalArray;
+using Data;
+using UnityEngine;
 
 public class MetricController : ChristoffelElement {
     private MetricView View => App.view.menu.metricSelection.metric;
+
+    private TensorProperties tensorProperties;
+    private string laTeXOfMetric;
+    private Texture2D texture;
     private void Awake() {
-        App.spaceDataChanged.listOfHandlers += async(caller, e) => {
-            if (e.tensorProperties.Metric.GetLength(0) > 0) {
-                string matrixTeX = e.tensorProperties.Metric.ToJagged().Aggregate<string[], string>(
-                    "",
-                    (string acc, string[] curr) => {
-                        return $"{acc}{curr.Aggregate((acc2, curr2) => $"{acc2} & {curr2}")}\\\\\n";
-                    }
-                );
-                string teX = $"g_{{\\mu\\nu}} = \\begin{{bmatrix}}{matrixTeX}\\end{{bmatrix}}";
-                Texture2D texture = await LaTeXTextureDownloader.FetchOneTexture(teX);
-                View.Texture = texture;
-            }
-        };
+        App.spaceDataChanged.listOfHandlers += OnSpaceDataChanged;
+    }
+
+    private void OnSpaceDataChanged(object caller, SpaceChangedArgs e) {
+        tensorProperties = e.tensorProperties;
+        if (HasTensorAMetric()) FetchAndShowMetric();
+    }
+
+    private bool HasTensorAMetric() {
+        return tensorProperties.Metric.GetLength(0) > 0;
+    }
+
+    private async void FetchAndShowMetric() {
+        laTeXOfMetric = GenerateLaTeXOfMetric();
+        await FetchTexture();
+        ShowMetric();
+    }
+
+    private string GenerateLaTeXOfMetric() {
+        string matrixBody = GenerateLaTeXOfBodyOfMatrix();
+        return GenerateLaTeXOfMetricFromBody(matrixBody);
+    }
+
+    private string GenerateLaTeXOfBodyOfMatrix() {
+        return tensorProperties.Metric
+            .ToJagged()
+            .Select(row => row.Aggregate(ConnectCellsIntoRow))
+            .Aggregate(ConnectRowsIntoTable);
+    }
+
+    private string ConnectCellsIntoRow(string previousCells, string currentCells) {
+        return $"{previousCells} & {currentCells}";
+    }
+
+    private string ConnectRowsIntoTable(string previousRows, string currentRow) {
+        return $"{previousRows}\\\\\n{currentRow}";
+    }
+
+    private string GenerateLaTeXOfMetricFromBody(string matrixBody) {
+        return $"g_{{\\mu\\nu}} = \\begin{{bmatrix}}{matrixBody}\\end{{bmatrix}}";
+    }
+
+    private async Task FetchTexture() {
+        texture = await LaTeXTextureDownloader.FetchOneTexture(laTeXOfMetric);
+    }
+
+    private void ShowMetric() {
+        View.Texture = texture;
     }
 }
