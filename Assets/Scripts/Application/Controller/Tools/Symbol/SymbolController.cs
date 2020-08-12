8b02ad58e69cb9adfec82cb8cb0588ac9c874292
deBroglie <tanski.gustaw@gmail.com>
2020-08-12 15:28:52 +0200

    Cleaned

--- Assets/Scripts/Application/Controller/Tools/Symbol/SymbolController.cs
+++ Assets/Scripts/Application/Controller/Tools/Symbol/SymbolController.cs
@@ -1,37 +1,50 @@
-using UnityEngine;
+using System.Threading.Tasks;
 using Data;
+using UnityEngine;
 public class SymbolController : ChristoffelElement {
     private SymbolView View => App.view.tools.symbol;
 
     private TensorProperties tensorProperties;
+    private string laTeX;
+    private Texture2D texture;
 
     private void Awake() {
-        App.spaceVisualizedByCubeChanged.listOfHandlers += async (sender, e) => {
-            string laTeX = SymbolIndexToLaTeXConverter.Convert(e.tensorProperties);
-            View.HideSymbol();
-            View.ShowLoader();
-            Texture2D texture = await LaTeXTextureDownloader.FetchOneTexture(laTeX);
-            View.Texture = texture;
-            View.HideLoader();
-            View.ShowSymbol();
-        };
-    }
-}
-
-public class SymbolIndexToLaTeXConverter : IndexToLaTeXConverter {
-
-    static public string Convert(TensorProperties props) {
-        return new SymbolIndexToLaTeXConverter(props).GetLaTeX();
+        App.spaceVisualizedByCubeChanged.listOfHandlers += OnSpaceVisualizedByCubeChanged;
     }
 
-    public SymbolIndexToLaTeXConverter(TensorProperties props) : base(props) {}
+    private void OnSpaceVisualizedByCubeChanged(object sender, SpaceChangedArgs e) {
+        tensorProperties = e.tensorProperties;
+        FetchAndShowSymbol();
+    }
 
+    private async void FetchAndShowSymbol() {
+        View.HideSymbol();
+        View.ShowLoader();
+        string nameOfFetched = tensorProperties.Name;
+        GenerateLaTeX();
+        await FetchTexture();
+        IfNameOfFetchedIsCurrentTensorNameUpdateSymbol(nameOfFetched);
+    }
 
-    protected override IndexWrapper ConvertToWrappers(TensorProperties.Index index, int n) {
-        return new IndexWrapper {
-            CoordinateLaTeX = index.LaTeX,
-                Number = n,
-                Index = index
-        };
+    private void GenerateLaTeX() {
+        laTeX = SymbolIndexToLaTeXConverter.Convert(tensorProperties);
+    }
+
+    private async Task FetchTexture() {
+        texture = await LaTeXTextureDownloader.FetchOneTexture(laTeX);
+    }
+
+    private void IfNameOfFetchedIsCurrentTensorNameUpdateSymbol(string nameOfFetched) {
+        if (IsNameOfFetchedTheCurrentTensorName(nameOfFetched)) UpdateSymbol();
+    }
+
+    private bool IsNameOfFetchedTheCurrentTensorName(string nameOfFetched) {
+        return nameOfFetched == tensorProperties.Name;
+    }
+
+    private void UpdateSymbol() {
+        View.Texture = texture;
+        View.HideLoader();
+        View.ShowSymbol();
     }
 }
