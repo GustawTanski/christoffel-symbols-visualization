6ead06f6a8530ca42fb933d951b0604d0baed4f0
deBroglie <tanski.gustaw@gmail.com>
2020-08-15 15:50:57 +0200

    Added Minkowski, Schwarzschild, de Sitter, Reissner-Nordstrom and Kerr. One major performence issue fixed (sprite creating)

--- Assets/Scripts/Component/Dynamic/SpriteCreator.cs
+++ Assets/Scripts/Component/Dynamic/SpriteCreator.cs
@@ -6,7 +6,9 @@ static public class SpriteCreator {
             texture,
             new Rect(0, 0, texture.width, texture.height),
             new Vector2(0.5f, 0.5f),
-            pixelsPerUnit
+            pixelsPerUnit,
+            0,
+            SpriteMeshType.FullRect
         );
     }
 }
