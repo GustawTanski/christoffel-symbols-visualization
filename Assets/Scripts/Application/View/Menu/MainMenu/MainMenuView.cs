b21c425463a585f70c916ce588e200791213715b
deBroglie <tanski.gustaw@gmail.com>
2020-09-01 10:53:24 +0200

    Main Menu's title added

--- Assets/Scripts/Application/View/Menu/MainMenu/MainMenuView.cs
+++ Assets/Scripts/Application/View/Menu/MainMenu/MainMenuView.cs
@@ -1,3 +1,10 @@
-public class MainMenuView : MenuElement {
+using TMPro;
+using UnityEngine;
 
+public class MainMenuView : MenuElement {
+    public TMP_Text header;
+
+    private void Awake() {
+        header.text = $"<color=#FFC000>Welcome to Christoffel symbol visualisation</color>\n<color=#9F9FDF><size=-30> v. {Application.version}</size></color>";
+    }
 }
